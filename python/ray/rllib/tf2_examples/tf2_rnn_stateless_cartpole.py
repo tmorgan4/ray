1a1d42fd543dc770d647bdd3fadcfd2e31c330fc
TJM <tmorgan434@gmail.com>
2019-08-15 18:58:19 -0600

    stateless cartpole debugging env changes

--- python/ray/rllib/tf2_examples/tf2_rnn_stateless_cartpole.py
+++ python/ray/rllib/tf2_examples/tf2_rnn_stateless_cartpole.py
@@ -8,13 +8,15 @@ import ray
 from ray import tune
 from ray.rllib.models.tf.tf_modelv2 import TFModelV2
 from ray.rllib.models import ModelCatalog
-from ray.rllib.models.lstm import add_time_dimension
+from ray.rllib.policy.rnn_sequencing import add_time_dimension
 import numpy as np
-import tensorflow as tf
 import math
 import gym
 from gym import spaces, logger
 from gym.utils import seeding
+from ray.rllib.utils import try_import_tf
+tf = try_import_tf()
+
 
 ### misc suggestions ###
 # Standardize use of seq_len and seq_lens (both are used, leading to confusion)
@@ -58,6 +60,34 @@ class CartPoleStatelessEnv(gym.Env):
     """ Partially observed variant of the CartPole gym environment.
     We delete the velocity component of the state, so that it can only be solved by a LSTM policy.
 
+    Original CartPole-v0 (unmodified) Specs:
+        Observation:
+            Type: Box(4)
+            Num	Observation	Min	Max
+            0	Cart Position	-2.4	2.4
+            1	Cart Velocity	-Inf	Inf
+            2	Pole Angle	~ -41.8°	~ 41.8°
+            3	Pole Velocity At Tip	-Inf	Inf
+
+        Actions:
+            Type: Discrete(2)
+            Num	Action
+            0	Push cart to the left
+            1	Push cart to the right
+
+        Stateless CartPole (this env) Specs:
+        Observation:
+            Type: Box(2)
+            Num	Observation	Min	Max
+            0	Cart Position	-2.4	2.4
+            2	Pole Angle	~ -41.8°	~ 41.8°
+
+        Actions:
+            Type: Discrete(2)
+            Num	Action
+            0	Push cart to the left
+            1	Push cart to the right
+
     Env copied from this example with minor changes:
     https://github.com/ray-project/ray/blob/master/python/ray/rllib/examples/cartpole_lstm.py """
 
@@ -77,9 +107,11 @@ class CartPoleStatelessEnv(gym.Env):
         self.theta_threshold_radians = 12 * 2 * math.pi / 360
         self.x_threshold = 2.4
 
-        high = np.array([self.x_threshold * 2,self.theta_threshold_radians * 2])
-        self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Box(-high, high)
+        high = np.array([self.x_threshold * 2, self.theta_threshold_radians * 2])
+        # self.action_space = spaces.Discrete(2)
+        self.action_space = spaces.MultiDiscrete([2])
+        self.observation_space = spaces.Dict({'cart_position': spaces.Box(-high[0], high[0], dtype=np.float32),
+                                              'pole_angle': spaces.Box(-high[1], high[1], dtype=np.float32)})
         self.seed()
         self.viewer = None
         self.state = None
@@ -123,13 +155,15 @@ class CartPoleStatelessEnv(gym.Env):
             self.steps_beyond_done += 1
             reward = 0.0
 
-        rv = np.r_[self.state[0], self.state[2]]
+        # rv = np.r_[self.state[0], self.state[2]]
+        rv = {'cart_position': self.state[0], 'pole_angle': self.state[2]}
         return rv, reward, done, {}
 
     def reset(self):
         self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4, ))
         self.steps_beyond_done = None
-        rv = np.r_[self.state[0], self.state[2]]
+        # rv = np.r_[self.state[0], self.state[2]]
+        rv = {'cart_position': self.state[0], 'pole_angle': self.state[2]}
         return rv
 
 
@@ -168,7 +202,7 @@ class MaskingLayerRNNmodel(TFModelV2):
             name='inputLayer')
         dense_layer_1 = tf.keras.layers.Dense(
             model_config['fcnet_hiddens'][0],
-            activation=tf.nn.relu,
+            activation=tf.nn.tanh,
             name='denseLayer1')(self.input_layer)
         masking_layer = tf.keras.layers.Masking(
             mask_value=0.0)(dense_layer_1)
@@ -185,7 +219,7 @@ class MaskingLayerRNNmodel(TFModelV2):
             target_shape=(model_config['lstm_cell_size'],))(lstm_out)
         dense_layer_2 = tf.keras.layers.Dense(
             model_config['fcnet_hiddens'][1],
-            activation=tf.nn.relu,
+            activation=tf.nn.tanh,
             name='denseLayer2')(reshape_layer)
         logits_layer = tf.keras.layers.Dense(
             self.num_outputs,
@@ -203,7 +237,7 @@ class MaskingLayerRNNmodel(TFModelV2):
 
     # Implement the core forward method
     def forward(self, input_dict, state, seq_lens):
-        x = input_dict['obs']
+        x = input_dict['obs_flat']
         if x._rank() < 3:
             x = add_time_dimension(x, seq_lens)
         logits, self._value_out, state = self.base_model(x)
@@ -251,7 +285,7 @@ class SequenceMaskRNNmodel(TFModelV2):
             name='reshapeLayer')
         self.dense_layer_2 = tf.keras.layers.Dense(
             units=model_config['fcnet_hiddens'][1],
-            activation=tf.nn.relu,
+            activation=tf.nn.tanh,
             name='denseLayer2')
         self.logits_layer = tf.keras.layers.Dense(
             units=self.num_outputs,
@@ -329,7 +363,7 @@ class InputLayerRNNmodel(TFModelV2):
         state_c_in = tf.keras.layers.Input(batch_shape=(None, model_config['lstm_cell_size']), name='state_c_in')
         dense_layer_1 = tf.keras.layers.Dense(
             model_config['fcnet_hiddens'][0],
-            activation=tf.nn.relu,
+            activation=tf.nn.tanh,
             name='denseLayer1')(inputs)
         mask = tf.sequence_mask(seq_lens, model_config['max_seq_len']) if model_config['max_seq_len'] > 1 else None
         lstm = tf.keras.layers.LSTM(
@@ -338,14 +372,14 @@ class InputLayerRNNmodel(TFModelV2):
             return_state=True,
             name='lstmLayer')
         lstm_outputs, state_h_out, state_c_out = lstm(inputs=dense_layer_1,
-                                              mask=mask,
-                                              initial_state=[state_h_in, state_c_in] if self.initialize_lstm_with_prev_state is True else None)
+                                                      mask=mask,
+                                                      initial_state=[state_h_in, state_c_in] if self.initialize_lstm_with_prev_state is True else None)
         # reshape_layer = tf.reshape(lstm[0], [-1, model_config['lstm_cell_size']])
         reshape_layer = tf.keras.layers.Reshape(
             target_shape=(model_config['lstm_cell_size'],))(lstm_outputs)
         dense_layer_2 = tf.keras.layers.Dense(
             model_config['fcnet_hiddens'][1],
-            activation=tf.nn.relu,
+            activation=tf.nn.tanh,
             name='denseLayer2')(reshape_layer)
         logits_layer = tf.keras.layers.Dense(
             self.num_outputs,
@@ -388,24 +422,17 @@ class InputLayerRNNmodel_v2(TFModelV2):
     def __init__(self, obs_space, action_space, num_outputs, model_config, name, **kw):
         super(InputLayerRNNmodel_v2, self).__init__(obs_space, action_space, num_outputs, model_config, name, **kw)
         self.initialize_lstm_with_prev_state = model_config['custom_options']['initialize_lstm_with_prev_state']
-        self.input_layer = tf.keras.layers.Input(
-            shape=(None, obs_space.shape[0]),
-            name='inputLayer')
-        self.state_in_c = tf.keras.layers.Input(
-            shape=(model_config['lstm_cell_size']),
-            name='c')
-        self.state_in_h = tf.keras.layers.Input(
-            shape=(model_config['lstm_cell_size']),
-            name='h')
-        self.seq_in = tf.keras.layers.Input(
-            shape=(),
-            name='seqLens')
-        dense_layer_1 = tf.keras.layers.Dense(
-            model_config['fcnet_hiddens'][0],
-            activation=tf.nn.relu,
-            name='denseLayer1')(self.input_layer)
-#        masking_layer = tf.keras.layers.Masking(
-#            mask_value=0.0)(dense_layer_1)
+        self.input_layer = tf.keras.layers.Input(shape=(None, obs_space.shape[0]),
+                                                 name='inputLayer')
+        self.state_in_c = tf.keras.layers.Input(shape=(model_config['lstm_cell_size']),
+                                                name='c_in')
+        self.state_in_h = tf.keras.layers.Input(shape=(model_config['lstm_cell_size']),
+                                                name='h_in')
+        self.seq_in = tf.keras.layers.Input(shape=(),
+                                            name='seqLens')
+        dense_layer_1 = tf.keras.layers.Dense(model_config['fcnet_hiddens'][0],
+                                              activation=tf.nn.tanh,
+                                              name='denseLayer1')(self.input_layer)
         lstm_out, state_h, state_c = tf.keras.layers.LSTM(
             model_config['lstm_cell_size'],
             return_sequences=True,
@@ -413,10 +440,9 @@ class InputLayerRNNmodel_v2(TFModelV2):
             name='lstmLayer')(inputs=dense_layer_1,
                               mask=tf.sequence_mask(self.seq_in) if self.model_config['max_seq_len'] > 1 else None,
                               initial_state=[self.state_in_c, self.state_in_h])
-        reshape_layer = tf.keras.layers.Lambda(lambda x: tf.reshape(x, [-1, model_config['lstm_cell_size']]))(lstm_out)
         dense_layer_2 = tf.keras.layers.Dense(
             model_config['fcnet_hiddens'][1],
-            activation=tf.nn.relu,
+            activation=tf.nn.tanh,
             name='denseLayer2')(lstm_out)
         logits_layer = tf.keras.layers.Dense(
             self.num_outputs,
@@ -426,9 +452,8 @@ class InputLayerRNNmodel_v2(TFModelV2):
             1,
             activation=None,
             name='valueLayer')(dense_layer_2)
-        state = [state_h, state_c]
 
-        self.base_model = tf.keras.Model(inputs=[self.input_layer, self.state_in_c, self.state_in_h, self.seq_in],
+        self.base_model = tf.keras.Model(inputs=[self.input_layer, self.seq_in, self.state_in_h, self.state_in_c],
                                          outputs=[logits_layer, value_layer, state_h, state_c])
         self.register_variables(self.base_model.variables)
         self.base_model.summary()
@@ -438,16 +463,26 @@ class InputLayerRNNmodel_v2(TFModelV2):
 
     # Implement the core forward method
     def forward(self, input_dict, state, seq_lens):
-        x = input_dict['obs']
+        x = input_dict['obs_flat']
         if x._rank() < 3:
             x = add_time_dimension(x, seq_lens)
-        logits, self._value_out, h, c = self.base_model((x, state[0], state[1], seq_lens))
-
-        return tf.reshape(logits, [-1, 2]), [h, c]
+        logits, self._value_out, h_state, c_state = self.base_model((x, seq_lens, state[0], state[1]))
+        return tf.reshape(logits, [-1, self.num_outputs]), [h_state, c_state]
 
     def get_initial_state(self):
-        return [np.zeros(self.model_config['lstm_cell_size'], np.float32),
-                np.zeros(self.model_config['lstm_cell_size'], np.float32)]
+        initializer = self.model_config['custom_options'].get('rnn_state_initializer', 'zeros')
+        if initializer == 'zeros':
+            return [np.zeros(self.model_config['lstm_cell_size'], np.float32),
+                    np.zeros(self.model_config['lstm_cell_size'], np.float32)]
+        elif initializer == 'random_uniform':
+            return [np.random.uniform(-1.0, 1.0, self.model_config['lstm_cell_size']).astype(np.float32),
+                    np.random.uniform(-1.0, 1.0, self.model_config['lstm_cell_size']).astype(np.float32)]
+        elif initializer == 'random_normal':
+            return [np.random.normal(0, 0.1, self.model_config['lstm_cell_size']).astype(np.float32),
+                    np.random.normal(0, 0.1, self.model_config['lstm_cell_size']).astype(np.float32)]
+        else:
+            print(f'RNN state initializer argument not recognized!')
+
 
     def value_function(self):
         return tf.reshape(self._value_out, [-1])
@@ -455,7 +490,7 @@ class InputLayerRNNmodel_v2(TFModelV2):
 
 if __name__ == "__main__":
     initialize_lstm_with_prev_state = True  # if True restore the state from prevous batch, otherwise inialize with zeros
-    lstm_cell_size = 64
+    lstm_cell_size = 32
 
     # uncomment one of these models, "SequenceMaskRNNmodel is the only model that works properly
     # default_model = "MaskingLayerRNNmodel"
@@ -480,22 +515,29 @@ if __name__ == "__main__":
               "timesteps_total": 1e6},
         config={
             "env": "CartPoleStateless",
-            "num_envs_per_worker": 4,
-            "num_workers": 0 if local_mode is True else 4,
+            "num_envs_per_worker": 1,
+            "num_workers": 0 if local_mode is True else 2,
             "num_gpus": 0,
             "num_sgd_iter": 3,
             "vf_loss_coeff": 1e-4,
             'observation_filter': 'MeanStdFilter',
             "horizon": 200,  # CartPoleStatelessEnv does not have built in time limit
             "model": {
+                "no_final_linear": True,
                 "custom_model": default_model,
-                "custom_options": {"initialize_lstm_with_prev_state": initialize_lstm_with_prev_state},
+                "custom_options": {"initialize_lstm_with_prev_state": initialize_lstm_with_prev_state,
+                                   "rnn_state_initializer": tune.grid_search([
+                                       'zeros',
+                                       # 'random_uniform',
+                                       # 'random_normal',
+                                   ])},
                 "fcnet_hiddens": [64, 64],
                 "lstm_cell_size": lstm_cell_size,
                 "use_lstm": False,
                 "state_shape": [lstm_cell_size, lstm_cell_size],
                 "vf_share_layers": True,
-                "max_seq_len": 7,
+                "max_seq_len": 3,
             }
-        })
+        }
+    )
 
